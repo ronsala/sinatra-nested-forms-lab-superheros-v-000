6f59fa97e6439485327a83825ac749eaf3a6e012
Ron Sala <ron.sala@gmail.com>
2018-07-12 06:26:28 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,7 +10,7 @@ class App < Sinatra::Base
 
   post '/teams' do
     @team = Team.new(params)
-
+binding.pry
     # params[:team][:members].each do |details|
     # @member = Member.new(args)
     # # end
--- app/models/team.rb
+++ app/models/team.rb
@@ -1,11 +1,12 @@
 class Team
-  attr_reader :team_name, :team_motto#, :members
+  attr_reader :team_name, :team_motto, :members
 
   def initialize(params)
+    @team_name = params[:team][:name]
+    @team_motto = params[:team][:motto]
+    @members = params[:team][:members]
+
     binding.pry
-    # @team_name = params[:team][:name]
-    # @team_motto = params[:team][:motto]
-    # binding.pry
 
   end
 end
