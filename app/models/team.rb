d71f288cacba58cad80205b11ef1e38c3811dc65
Ron Sala <ron.sala@gmail.com>
2018-07-13 02:55:13 +0000

    Done.

--- app/models/team.rb
+++ app/models/team.rb
@@ -6,17 +6,19 @@ class Team
     # binding.pry
     @team_motto = params[:team][:motto]
     @members = params[:team][:members]
-    @member1_name = ["team"]["members"][0]["name"]
-    @member1_power = ["team"]["members"][0]["power"]
-    @member1_bio = ["team"]["members"][0]["bio"]
-    @member2_name = ["team"]["members"][1]["name"]
-    @member2_power = ["team"]["members"][1]["power"]
-    @member2_bio = ["team"]["members"][1]["bio"]
-    @member3_name = ["team"]["members"][2]["name"]
-    @member3_power = ["team"]["members"][2]["power"]
-    @member3_bio = ["team"]["members"][2]["bio"]
+    # @member1_name = ["team"]["members"][]["name"]
 
-    # binding.pry
+    @member1_power = ["team"]["members"][0]["power"]
+    binding.pry
+
+    # @member1_bio = ["team"]["members"][0]["bio"]
+    # @member2_name = ["team"]["members"][1]["name"]
+    # @member2_power = ["team"]["members"][1]["power"]
+    # @member2_bio = ["team"]["members"][1]["bio"]
+    # @member3_name = ["team"]["members"][2]["name"]
+    # @member3_power = ["team"]["members"][2]["power"]
+    # @member3_bio = ["team"]["members"][2]["bio"]
+
 
   end
 end
