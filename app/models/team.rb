cfdfa9dd8b707ad24097297593886635fa335179
Ron Sala <ron.sala@gmail.com>
2018-07-14 23:39:17 +0000

    automatically backed up by learn

--- app/models/team.rb
+++ app/models/team.rb
@@ -21,16 +21,16 @@ class Team
     @team_motto = params[:team][:motto]
     @members = params[:team][:members]
 
-    @members.each do |m|
-      @member1_name = @members[0]["name"]
-      @member1_power = @members[0]["power"]
-      @member1_bio = @members[0]["bio"]
-      @member2_name = @members[1]["name"]
-      @member2_power = @members[1]["power"]
-      @member2_bio = @members[1]["bio"]
-      @member3_name = @members[2]["name"]
-      @member3_power = @members[2]["power"]
-      @member3_bio = @members[2]["bio"]
+    @members.each_with_index do | m, i |
+      @member1_name = @members[i]["name"]
+      @member1_power = @members[i]["power"]
+      @member1_bio = @members[i]["bio"]
+      @member2_name = @members[i]["name"]
+      @member2_power = @members[i]["power"]
+      @member2_bio = @members[i]["bio"]
+      @member3_name = @members[i]["name"]
+      @member3_power = @members[i]["power"]
+      @member3_bio = @members[i]["bio"]
     end
   end
 end
